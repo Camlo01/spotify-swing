1788b35bcdb997f97b9a25d57464e2aa8748142b
camlo01 <camilobeltran174@gmail.com>
2022-12-28 17:16:07 -0500

    Indicated new storage path | commented panelRightBody

--- src/main/java/com/musica/musicar/view/GUI/jPanelBody/PanelBody.java
+++ src/main/java/com/musica/musicar/view/GUI/jPanelBody/PanelBody.java
@@ -1,12 +1,14 @@
 package com.musica.musicar.view.GUI.jPanelBody;
 
+import com.musica.musicar.view.GUI.jPanelBody.central.PanelBodyCentral;
+
 import javax.swing.*;
 
 public class PanelBody extends javax.swing.JPanel {
 
     private PanelLeftBody panelLeftBody = new PanelLeftBody();
     private PanelBodyCentral panelBodyCentral = new PanelBodyCentral();
-    private PanelRightBody panelRightBody = new PanelRightBody();
+//    private PanelRightBody panelRightBody = new PanelRightBody();
 
     public PanelBody() {
         initComponents();
@@ -22,13 +24,13 @@ public class PanelBody extends javax.swing.JPanel {
                                 .addComponent(panelLeftBody, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, 0)
                                 .addComponent(panelBodyCentral, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGap(0, 0, 0)
-                                .addComponent(panelRightBody, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
+                                .addGap(0, 0, 0)));
+//                                .addComponent(panelRightBody, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+//        );
         panelBodyLayout.setVerticalGroup(
                 panelBodyLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addComponent(panelLeftBody, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(panelRightBody, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+//                        .addComponent(panelRightBody, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(panelBodyCentral, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
