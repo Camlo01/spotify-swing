55409d643a7ad9f308d52157693761bac2f88aa3
camlo01 <camilobeltran174@gmail.com>
2023-02-01 23:28:58 -0500

    ADDED logic to resize the window | IMPROVED manage Window() to minimize the window to its previous position and size

--- src/main/java/com/musica/musicar/view/GUI/SpotifyApp.java
+++ src/main/java/com/musica/musicar/view/GUI/SpotifyApp.java
@@ -1,5 +1,6 @@
 package com.musica.musicar.view.GUI;
 
+
 import com.musica.musicar.view.GUI.jPanelBody.PanelBody;
 import com.musica.musicar.view.GUI.jPanelBottomBar.PanelBottomBar;
 import com.musica.musicar.view.GUI.jPanelTopBar.JPanelTopBar;
@@ -8,6 +9,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ * Main window of the application with all the panels to show
+ */
 public class SpotifyApp extends javax.swing.JFrame {
 
     //    buttons to manage this JFrame
@@ -18,17 +22,50 @@ public class SpotifyApp extends javax.swing.JFrame {
     //    value that stores the state of the window
     private boolean isFullOpen;
 
-    //    mouse position
-    private int mouseX, mouseY;
+    //    mouse position for move title bar
+    private int positionX, positionY;
+
+    //      variables for resize screen
+    private static final double RESIZE_MARGIN = 5.0;
+
+    //  Cursors to define the mouse position
+    private static final Cursor DEFAULT_CURSOR = new Cursor(Cursor.DEFAULT_CURSOR);
+    private static final Cursor TOP_CURSOR = new Cursor(Cursor.N_RESIZE_CURSOR);
+    private static final Cursor RIGHT_CURSOR = new Cursor(Cursor.E_RESIZE_CURSOR);
+    private static final Cursor BOTTOM_CURSOR = new Cursor(Cursor.S_RESIZE_CURSOR);
+    private static final Cursor LEFT_CURSOR = new Cursor(Cursor.W_RESIZE_CURSOR);
+    private static final Cursor DIAGONAL_NW_CURSOR = new Cursor(Cursor.NW_RESIZE_CURSOR);
+    private static final Cursor DIAGONAL_NE_CURSOR = new Cursor(Cursor.NE_RESIZE_CURSOR);
+    private static final Cursor DIAGONAL_SE_CURSOR = new Cursor(Cursor.SE_RESIZE_CURSOR);
+    private static final Cursor DIAGONAL_SW_CURSOR = new Cursor(Cursor.SW_RESIZE_CURSOR);
+
+    //  minimum sizes
+    private static final int MINIMIUM_WIDTH = 800;
+    private static final int MINIMIUM_HEIGHT = 600;
+
+
+    // Values that store the last size and position value of the window
+    // They are also used for window position and resizing logic
+    private int lBeforeX; // last X axis location
+    private int lBeforeY; // last Y axis location
+    private int wBeforeWidth = 800; // last window width measurement
+    private int wBeforeHeight = 600; // last window height measurement
+
+
+    // Variable declared to be accessible to the class from a block of code
+    private final JFrame THIS_JFRAME;
+
+    {
+        THIS_JFRAME = this;
+    }
 
     public SpotifyApp() {
         initComponents();
-        setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
     private void initComponents() {
-        isFullOpen = false;
+        isFullOpen = false; // The isFullOpen value is initialized to false because the window is opened minimized
 
 //        Construct panels
         panelBottomBar = new PanelBottomBar();
@@ -51,24 +88,8 @@ public class SpotifyApp extends javax.swing.JFrame {
             }
         });
 
-//        Setting behaviors to the panelTopBar for can move it
-        panelTopBar.addMouseMotionListener(new MouseMotionAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                setLocation(getX() + e.getX() - mouseX, getY() + e.getY() - mouseY);
-            }
-        });
+        //Configuring this JFrame
 
-        panelTopBar.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                mouseX = e.getX();
-                mouseY = e.getY();
-            }
-        });
-
-        panelBody.setMinimumSize(new Dimension(800, 524));
-//        Hide title bar of this JFrame
         setUndecorated(true);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -86,43 +107,213 @@ public class SpotifyApp extends javax.swing.JFrame {
                                 .addComponent(panelBody, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
                                 .addComponent(panelBottomBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
         );
-//        setMinimumSize(new Dimension(700, 800));
         setBackground(Color.red);
 
         pack();
-//        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getRootPane().setWindowDecorationStyle(JRootPane.WHEN_FOCUSED);
+        setLocationRelativeTo(null);
 
+        THIS_JFRAME.addMouseMotionListener(new MouseAdapter() {
+            int lBeforeX = (int) getLocation().getX();
+            int lBeforeY = (int) getLocation().getY();
+            int wBeforeWidth = getWidth();
+            int wBeforeHeight = getHeight();
+
+            /**
+             * The cursor style is set according to the captured position
+             * @param e MouseEvent
+             */
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                if (!isFullOpen) {
+                    double width = getWidth();
+                    double height = getHeight();
+                    double XAxis = e.getX();
+                    double YAxis = e.getY();
+                    setCursorWithParameters(width, height, XAxis, YAxis);
+                }
+            }
+
+            /**
+             * Depending on where the mouse position moves while it is being pressed,
+             * it will resize from the pressed edge or the Jframe will move.
+             * @param e MouseEvent
+             */
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+                int mouseX = e.getX();
+                int mouseY = e.getY();
+
+//                DIAGONALS ---
+                if (getCursor() == DIAGONAL_NW_CURSOR) {
+                    int newWidth = wBeforeWidth + (e.getX() * -1);
+                    int newXAxis = lBeforeX + e.getX();
+                    int newYAxis = (int) getLocation().getY() + mouseY;
+                    int newHeight = newYAxis * -1 + (wBeforeHeight + lBeforeY);
+
+//                            Set width and height
+                    if (newWidth >= MINIMIUM_WIDTH && newHeight >= MINIMIUM_HEIGHT) {
+                        setLocation(newXAxis, newYAxis);
+                        setSize(newWidth, newHeight);
+
+//                            Set only height if width is minimal
+                    } else if (newHeight >= MINIMIUM_HEIGHT && newWidth < MINIMIUM_WIDTH) {
+                        setLocation(getX(), newYAxis);
+                        setSize(getWidth(), Math.max(newHeight, MINIMIUM_HEIGHT));
+
+//                            Set width only if height is minimal
+                    } else if (newWidth >= MINIMIUM_WIDTH && newHeight < MINIMIUM_HEIGHT) {
+                        setSize(Math.max(newWidth, MINIMIUM_WIDTH), getHeight());
+                        setLocation(newXAxis, getY());
+                    }
+
+                } else if (getCursor() == DIAGONAL_NE_CURSOR) {
+                    int newWidth = lBeforeX + (mouseX - lBeforeX);
+                    int newYAxis = (int) getLocation().getY() + mouseY;
+                    int maxEdge = (wBeforeHeight + lBeforeY);
+                    int newHeight = newYAxis * -1 + maxEdge;
+
+                    if (newWidth >= MINIMIUM_WIDTH && newHeight >= MINIMIUM_HEIGHT) {
+                        setLocation(getX(), newYAxis);
+                        setSize(newWidth, newHeight);
+
+                    } else if (newWidth < MINIMIUM_WIDTH && newHeight >= MINIMIUM_HEIGHT) {
+                        setLocation(getX(), newYAxis);
+                        setSize(getWidth(), newHeight);
+
+                    } else {
+                        setSize(Math.max(newWidth, MINIMIUM_WIDTH), getHeight());
+                    }
+
+                } else if (getCursor() == DIAGONAL_SW_CURSOR) {
+                    int newHeight = lBeforeY + (mouseY - lBeforeY);
+                    int newWidth = wBeforeWidth + (e.getX() * -1);
+                    int newXAxis = lBeforeX + e.getX();
+
+                    if (newWidth >= MINIMIUM_WIDTH && newHeight >= MINIMIUM_HEIGHT) {
+                        setSize(newWidth, newHeight);
+                        setLocation(newXAxis, lBeforeY);
+
+                    } else if (newWidth < MINIMIUM_WIDTH) {
+                        setSize(wBeforeWidth, Math.max(MINIMIUM_HEIGHT, newHeight));
+
+                    } else {
+                        setSize(newWidth, wBeforeHeight);
+                        setLocation(newXAxis, lBeforeY);
+                    }
+
+                } else if (getCursor() == DIAGONAL_SE_CURSOR) {
+                    int newWidth = lBeforeX + (mouseX - lBeforeX);
+                    int newHeight = lBeforeY + (mouseY - lBeforeY);
+
+                    if (newWidth >= MINIMIUM_WIDTH && newHeight >= MINIMIUM_HEIGHT) {
+                        setSize(newWidth, newHeight);
+
+                    } else if (newWidth < MINIMIUM_WIDTH) {
+                        setSize(MINIMIUM_WIDTH, Math.max(newHeight, MINIMIUM_HEIGHT));
+
+                    } else {
+                        setSize(newWidth, MINIMIUM_HEIGHT);
+                    }
+
+//                EDGED ---
+
+                } else if (getCursor() == TOP_CURSOR) {
+                    int newYAxis = (int) getLocation().getY() + mouseY;
+                    int newHeight = newYAxis * -1 + (wBeforeHeight + lBeforeY);
+
+                    if (newHeight >= MINIMIUM_HEIGHT) {
+                        setSize(getWidth(), Math.max(newHeight, MINIMIUM_HEIGHT));
+                        setLocation(getX(), newYAxis);
+                    }
+                } else if (getCursor() == BOTTOM_CURSOR) {
+                    int newHeight = lBeforeY + (mouseY - lBeforeY);
+
+                    if (newHeight > MINIMIUM_HEIGHT) {
+                        setSize(getWidth(), newHeight);
+                    }
+                } else if (getCursor() == LEFT_CURSOR) {
+                    int newXAxis = (int) getLocation().getX() + mouseX;
+                    int newWidth = (newXAxis * -1 + (wBeforeWidth + lBeforeX));
+
+                    if (newWidth >= MINIMIUM_WIDTH) {
+                        setSize(newWidth, getHeight());
+                        setLocation(newXAxis, getY());
+                    }
+                } else if (getCursor() == RIGHT_CURSOR) {
+                    int newWidth = lBeforeX + (mouseX - lBeforeX);
+
+                    if (newWidth >= MINIMIUM_WIDTH) {
+                        setSize(newWidth, getHeight());
+                    }
+                }
+
+                //It is validated that from the taskbar you are pressing to be able to move the window
+                if (getCursor() == DEFAULT_CURSOR && isClickedOnTopEdge(THIS_JFRAME, e)) {
+                    setLocation(getX() + e.getX() - positionX, getY() + e.getY() - positionY);
+                }
+
+//                updating values
+                lBeforeX = (int) getLocation().getX();
+                lBeforeY = (int) getLocation().getY();
+                wBeforeWidth = getWidth();
+                wBeforeHeight = getHeight();
+            }
+        });
+
+        THIS_JFRAME.addMouseListener(new MouseAdapter() {
+            /**
+             * Capture mouse position on pressing the JFrame
+             * @param e MouseEvent
+             * @Override
+             */
+            @Override
+            public void mousePressed(MouseEvent e) {
+                positionX = e.getX();
+                positionY = e.getY();
+            }
+        });
     }
 
+    //-------------------------------------------------------
+    //------------------- MAIN METHOD------------------------
+    //-------------------------------------------------------
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 SpotifyApp app = new SpotifyApp();
                 app.setTitle("Spotify");
                 app.setVisible(true);
-//                app.setResizable(true);
             }
         });
     }
 
     /**
-     * Method in charge
+     * Method in charge of managing the logic of the button to maximize the window to show
+     * the full window or in the last size and position it had before being resized
      */
     private void manageWindow() {
         if (!isFullOpen) {
-//            setExtendedState(JFrame.MAXIMIZED_BOTH);
+//            The size and location values are saved before the window is maximized
+            lBeforeX = (int) getLocation().getX();
+            lBeforeY = (int) getLocation().getY();
+            wBeforeWidth = getWidth();
+            wBeforeHeight = getHeight();
+            //configure the size of the JFrame allowing to see the taskbar
             maximizedWindow();
         } else {
-            setSize(new Dimension(900, 800));
+            setSize(new Dimension(wBeforeWidth, wBeforeHeight));
             isFullOpen = false;
-            setLocationRelativeTo(null);
+            setLocation(new Point(lBeforeX, lBeforeY));
         }
     }
 
     /**
      * Method in charge of maximizing the JFrame allowing to show the taskbar
+     * and set the value of isFullOpen to true to disable the styles of the
+     * cursor on the borders, which allows its resizing
      */
     private void maximizedWindow() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -131,9 +322,54 @@ public class SpotifyApp extends javax.swing.JFrame {
         isFullOpen = true;
     }
 
+    /**
+     * Method in charge of verifying that it has been pressed from the title bar
+     *
+     * @param frame frame to evaluate
+     * @param e     MouseEvent to evaluate
+     * @return True if pressed from the title bar
+     */
+    private boolean isClickedOnTopEdge(JFrame frame, MouseEvent e) {
+        double MARGIN = RESIZE_MARGIN;
+        return (e.getX() > MARGIN && e.getX() <= frame.getWidth() - MARGIN &&
+                e.getY() > MARGIN && e.getY() <= 36);
+    }
+
+    /**
+     * Method in charge of setting the styles of the cursor according to the position that is captured
+     * Exam. when the mouse is over the top border the TOP_CURSOR cursor is set
+     *
+     * @param width  of JFrame
+     * @param height of JFrame
+     * @param XAxis  position of mouse
+     * @param YAxis  position of mouse
+     */
+    private void setCursorWithParameters(double width, double height, double XAxis, double YAxis) {
+        //                DIAGONALS ---
+        if (YAxis <= RESIZE_MARGIN && XAxis < RESIZE_MARGIN + 1) { // Diagonal TOP-left
+            setCursor(DIAGONAL_NW_CURSOR);
+        } else if (YAxis <= RESIZE_MARGIN && XAxis >= width - RESIZE_MARGIN) { // Diagonal TOP-right
+            setCursor(DIAGONAL_NE_CURSOR);
+        } else if (YAxis >= height - RESIZE_MARGIN && XAxis <= RESIZE_MARGIN) { // Diagonal BOTTOM-left
+            setCursor(DIAGONAL_SW_CURSOR);
+        } else if (YAxis >= height - RESIZE_MARGIN && XAxis >= width - RESIZE_MARGIN) { // Diagonal BOTTOM-right
+            setCursor(DIAGONAL_SE_CURSOR);
+//                    EDGES ---
+        } else if (YAxis <= RESIZE_MARGIN && (XAxis > RESIZE_MARGIN && XAxis < width - RESIZE_MARGIN)) { // TOP Edge
+            setCursor(TOP_CURSOR);
+        } else if (YAxis > height - RESIZE_MARGIN && (XAxis > RESIZE_MARGIN && XAxis < width - RESIZE_MARGIN)) { // BOTTOM Edge
+            setCursor(BOTTOM_CURSOR);
+        } else if (XAxis <= RESIZE_MARGIN && (YAxis > RESIZE_MARGIN && YAxis < height - RESIZE_MARGIN)) { // LEFT Edge
+            setCursor(LEFT_CURSOR);
+        } else if (XAxis > width - RESIZE_MARGIN && (YAxis > RESIZE_MARGIN && YAxis < height - RESIZE_MARGIN)) { // RIGHT Edge
+            setCursor(RIGHT_CURSOR);
+        } else {
+            setCursor(DEFAULT_CURSOR);
+        }
+    }
+
     private JPanelTopBar panelTopBar;
     private PanelBottomBar panelBottomBar;
     private PanelBody panelBody;
-
 
 }
