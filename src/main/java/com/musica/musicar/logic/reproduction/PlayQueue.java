5a87664a897110f1de00167552713ef8bacd2e8c
camlo01 <camilobeltran174@gmail.com>
2023-02-01 23:39:59 -0500

    class that will handle the playback queue

---
+++ src/main/java/com/musica/musicar/logic/reproduction/PlayQueue.java
@@ -0,0 +1,17 @@
+package com.musica.musicar.logic.reproduction;
+
+import java.util.ArrayList;
+
+/**
+ * Class in charge of managing the logic of the following files to be reproduced
+ */
+public class PlayQueue {
+
+    private final ArrayList<String> queue = new ArrayList<>();
+
+    public PlayQueue() {
+
+    }
+
+
+}
